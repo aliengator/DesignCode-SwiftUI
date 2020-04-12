28ad04fe0c0cc71eba00ee73da60b9cb1e557ed9
aliengator <aquatomics@gmail.com>
2020-04-11 22:50:56 -0500

    Basic Components

--- DesignCode-app/DesignCode-app/ContentView.swift
+++ DesignCode-app/DesignCode-app/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  DesignCode-app
 //
@@ -10,7 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            TitleView()
+            .blur(radius: 20)
+            
+            BackCardView()
+            .background(Color("card4"))
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -40)
+            .scaleEffect(0.90)
+            .rotationEffect(.degrees(10))
+            .rotation3DEffect(Angle.degrees(10), axis: (x: 10, y: 0, z: 0))
+            .blendMode(.hardLight)
+            
+            BackCardView()
+            .background(Color("card3"))
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            .scaleEffect(0.95)
+            .rotationEffect(.degrees(5))
+            .rotation3DEffect(Angle.degrees(5), axis: (x: 10, y: 0, z: 0))
+            .blendMode(.hardLight)
+            
+            CardView()
+            .blendMode(.hardLight)
+            
+            BottomCardView()
+            .blur(radius: 20)
+        }
     }
 }
 
@@ -19,3 +48,85 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
+ struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 20)
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+ }
+ 
+ 
+ struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+    }
+ }
+ 
+ 
+ struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+ }
+ 
+ 
+ struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(5)
+                .opacity(0.1)
+            Text("This certificate is proof that Sam Alarcón has achieved the UI Design Course with approval from a Design+Code instructor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 550)
+    }
+ }
+ 
